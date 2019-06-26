0be3708e144383722cd13f30fa6d04313e4b582e
samihda <samihda@users.noreply.github.com>
2019-06-26 23:54:14 +0200

    Adjust output formatting

--- if/Ls.java
+++ if/Ls.java
@@ -14,7 +14,7 @@ public class Ls {
     private static void displayInfo(NetworkInterface iface) throws SocketException {
         Enumeration<InetAddress> addresses = iface.getInetAddresses();
 
-        System.out.printf("Name: %s\n", iface.getName());
+        System.out.printf("%s\n", iface.getName());
 
         for (InetAddress address: Collections.list(addresses)) {
             System.out.printf("Address: %s\n", address);
