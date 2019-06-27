7a2e6afda2db7ff91917aef22b4599e1fa4c75b0
samihda <samihda@users.noreply.github.com>
2019-06-28 01:34:13 +0200

    Integrate separate class file

--- echo/EchoServer.java
+++ echo/EchoServer.java
@@ -1,6 +1,34 @@
 import java.net.*;
 import java.io.*;
 
+class EchoServerThread extends Thread {
+    private Socket socket = null;
+
+    public EchoServerThread(Socket socket) {
+        super("EchoThread");
+        this.socket = socket;
+    }
+
+    public void run() {
+        try (
+             PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
+             BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+         ) {
+            socketOut.println("Welcome!");
+
+            String reply;
+
+            while ((reply = socketIn.readLine()) != null) {
+                socketOut.println(reply);
+            }
+
+            socket.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
 public class EchoServer {
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
