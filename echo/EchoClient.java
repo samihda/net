44a8a2b9846feb037d410340b777725abaed94e9
samihda <samihda@users.noreply.github.com>
2019-06-27 19:41:18 +0200

    Support custom host

--- echo/EchoClient.java
+++ echo/EchoClient.java
@@ -3,8 +3,16 @@ import java.net.*;
 
 public class EchoClient {
     public static void main(String[] args) {
+        if (args.length != 2) {
+            System.err.println("Usage: java EchoClient <host> <port>");
+            System.exit(1);
+        }
+
+        String hostName = args[0];
+        int portNumber = Integer.parseInt(args[1]);
+
         try (
-            Socket socket = new Socket("127.0.0.1", 4444);
+            Socket socket = new Socket(hostName, portNumber);
             PrintWriter socketOut = new PrintWriter(socket.getOutputStream(), true);
             BufferedReader socketIn = new BufferedReader(new InputStreamReader(socket.getInputStream()));
         ) {
